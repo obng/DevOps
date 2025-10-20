252c8f6d06f52108e69f1db4ea1759d8d7f475d4
bong <p1-02@naver.com>
2025-10-20 11:30:40 +0900

    박봉근/Project init/25.10.20

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/board", (req, res) => {
+  res.send({title: "노드 api 서버 update!! "});
+});
+
+const path = require("path");
+const publicPath = path.join(__dirname, "public");
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+});
+
+// 에러 처리+
+app.use((req, res) => {
+  res.status(400).sendFile(path.join(__dirname, "./public", "index.html"));
+});
